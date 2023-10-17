a3a5123ba86c5aad587d18daa0ecc352172d3dec
userdiagonallley <006shubham006@gmail.com>
2023-10-17 12:34:44 +0530

    fetching list from db

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ import requests
 import os
 import sqlite3
 
-url = "https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Korean_5800"
-res = requests.get(url)
-res.encoding = "utf-8"
-
-
-soup = BeautifulSoup(res.text, features="html.parser")
-list_of_e = soup.select(".Kore")
 list_of_words = []
 
 
@@ -22,7 +15,7 @@ def get_words(words):
         c = connection.cursor()
         c.execute("Select * from words;")
         list_of_words = c.fetchall()
-        print(type(list_of_words))
+
         # c.execute("Create table words(word TEXT, href TEXT)")
 
         # for word in words:
@@ -36,32 +29,56 @@ def get_info(href):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
     latin = soup.select(".manual-tr")[0].get_text()
+
     print(latin)
     data = soup.select("ol")  # returns a list
+    i = 1
     for word in data:
         meaning = word.find("li").get_text()
-        print(meaning+"\n")
+        print(str(i)+". "+meaning+"\n")
+        i += 1
 
 
-for li in list_of_e:
-    word = {
-        "word": li.find("a")["title"],
-        "href": li.find("a")["href"]
-    }
-    if (word["word"].find("(page does not exist)")) == -1:
-        list_of_words.append(word)
+def get_list_of_words(list_of_words):
 
+    url = "https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Korean_5800"
+    res = requests.get(url)
+    res.encoding = "utf-8"
+
+    soup = BeautifulSoup(res.text, features="html.parser")
+    list_of_e = soup.select(".Kore")
+    list_of_words = []
+
+    for li in list_of_e:
+        word = {
+            "word": li.find("a")["title"],
+            "href": li.find("a")["href"]
+        }
+        if (word["word"].find("(page does not exist)")) == -1:
+            list_of_words.append(word)
+
+
+try:
+    get_words(list_of_words)
+except:
+    print("Excpetion while fetching from db, getting from other source!")
+    get_list_of_words(list_of_words)
 f = Figlet(width=100)
 
 print(f.renderText("Korean 5000 Frequency Words"))
-get_words(list_of_words)
+
+
 while True:
     word = choice(list_of_words)
+    if type(word) == "dict":
 
-    print(f"Here is one word: {word['word']}")
-    get_info(word["href"])
-    next = input("Press N for next, anything else to exit:")
-    if next.lower() == "n":
+        print(f"Here is one word: {word['word']}")
+        get_info(word["href"])
+    else:
+        print(f"Here is one word: {word[0]}")
+        get_info(word[1])
+    next = input("Press enter to continue, anything else to exit: ")
+    if next.lower() == "":
         os.system("cls")
         continue
     else:
